ee45689d6434f45eb69687f285489acde6ad3345
rutrarch <arturkummer08@gmail.com>
2026-05-24 16:59:14 +0200

    made it actually scale ig

--- main.cpp
+++ main.cpp
@@ -4,8 +4,8 @@
 
 
 int main() {
-    constexpr int screenWidth = 800;
-    constexpr int screenHeight = 600;
+    constexpr int screenWidth = 2800;
+    constexpr int screenHeight = 400;
 
     Game* game = new Game(screenWidth, screenHeight);
     game->init();
