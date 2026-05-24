a9c46bd6892c8dcb6f559e91a04a1d3bec977e66
rutrarch <arturkummer08@gmail.com>
2026-05-24 09:02:36 +0200

    raylib hewwo wowd

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include "raylib.h"
 
 int main() {
 #ifdef NDEBUG
@@ -6,6 +7,25 @@ int main() {
 #else
     std::cout << "Running in Debug mode\n";
 #endif
+    const int screenWidth = 2000;
+    const int screenHeight = 1000;
+
+    InitWindow(screenWidth, screenHeight, "major major major major");
+
+    while (!WindowShouldClose())
+    {
+        BeginDrawing();
+        ClearBackground(BLACK);
+#ifdef NDEBUG
+        DrawText("Release mode", 67, 67, 20, WHITE);
+#else
+        DrawText("Debug mode", 67, 67, 20, WHITE);
+        DrawFPS(10, 10);
+#endif
+        EndDrawing();
+    }
+    CloseWindow();
+
     std::cout << "arch btw\n";
     return 0;
 }
