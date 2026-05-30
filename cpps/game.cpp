510da06110ef01790abef5e570b47498bc98c880
mzdan <mzdanowiczm@gmail.com>
2026-05-30 22:36:52 +0200

    nowe melodyjki

--- cpps/game.cpp
+++ cpps/game.cpp
@@ -56,8 +56,11 @@ void Game::init() {
     int speed=1;
     //sequence=std::make_unique<Sequence>(std::vector{0,2,4,4,4,4,4,5,4,2,0,2,1,0,1,2},std::vector{100/speed,100/speed,50/speed,50/speed,50/speed,50/speed,100/speed,100/speed,100/speed,100/speed,100/speed,100/speed,200/speed,100/speed,100/speed,200/speed});
     //sequence=std::make_unique<Sequence>(std::vector{0,1},std::vector{100,100});
-    sequence=std::make_unique<Sequence>(std::vector{0,4,3,2,1,7,4,3,2,1,7,4,3,2,3,1},std::vector{50,50,10,10,10,50,50,10,10,10,50,50,15,15,15,100});
-    //sequence=std::make_unique<Sequence>(std::vector{0,1,2,1,2,3,2,1,0,0,4,0,0,0,0},std::vector{30,10,30,30,10,25,20,20,20,20,40,15,20,15,50});
+    //sequence=std::make_unique<Sequence>(std::vector{0,4,3,2,1,7,4,3,2,1,7,4,3,2,3,1},std::vector{50,50,10,10,10,50,50,10,10,10,50,50,15,15,15,100});
+    sequence=std::make_unique<Sequence>(std::vector{0,1,2,1,2,3,2,1,0,0,4,0,0,0,0},std::vector{30,10,30,30,10,25,20,20,20,20,40,15,15,15,50});
+    //sequence=std::make_unique<Sequence>(std::vector{0,0,4,4,5,5,4},std::vector{30,30,30,30,30,30,100});
+    //sequence=std::make_unique<Sequence>(std::vector{2,2,2,4,3,2,2,2,4,3},std::vector{30,30,30,20,20,30,30,30,20,20});
+
     std::cout<<sequence->completed<<std::endl;
     std::cout<<sequence->compleation_level<<std::endl;
 }
