c04e71b866c8a287d7ea271de736a48d62f1cf74
mzdan <mzdanowiczm@gmail.com>
2026-05-30 22:22:15 +0200

    nowe melodyjki

--- headers/config.h
+++ headers/config.h
@@ -2,7 +2,7 @@
 
 namespace config {
     inline constexpr bool enableBloom = false;
-    inline constexpr int screenWidth = 1000;
-    inline constexpr int screenHeight = 800;
+    inline constexpr int screenWidth = 1280;
+    inline constexpr int screenHeight = 720;
     constexpr double animation_frame_delay_ms = 64.0;
 }
