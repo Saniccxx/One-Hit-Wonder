a3b98307ec499d5ed8d6abefe90ef02f5dce715d
rutrarch <arturkummer08@gmail.com>
2026-06-02 19:05:51 +0200

    pwofesional loading scween

--- cpps/main.cpp
+++ cpps/main.cpp
@@ -14,6 +14,12 @@ int main() {
     const Config config;
     Renderer::init_audio_device();
     Renderer::init_window(config.get_screen_width(), config.get_screen_height(), "One hit wonder™");
+    int text_x = 10;
+    int text_y = 10;
+    Renderer::begin_drawing();
+    Renderer::clear_background(Renderer::black);
+    Renderer::draw_text("making sure the blob is exactly 13 years old", text_x, text_y, 24, Renderer::white);
+    Renderer::end_drawing();
     Game game(config);
     game.init();
     int hz = Renderer::get_monitor_refresh_rate();
