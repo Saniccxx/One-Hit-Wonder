c36f929686a5645871af20e9536a830cf6c7a82e
Sanuuu <shevchenko_oleksandr@outlook.com>
2026-06-03 21:59:05 +0200

    saving map to root folder

--- cpps/map_display_save_load.cpp
+++ cpps/map_display_save_load.cpp
@@ -21,7 +21,7 @@ void MapDisplay::saveMapToJson() {
         }
     }
 
-    std::ofstream file("map_data.json");
+    std::ofstream file("../map_data.json");
     file << json.dump(4);
     std::cout << "Saved map data.json" << std::endl;
 }
