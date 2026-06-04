e235024a3b77befc0877dbcffa48fe0beaff16a0
Sanuuu <shevchenko_oleksandr@outlook.com>
2026-06-04 15:19:55 +0200

    fixed pause system

--- cpps/pause_display.cpp
+++ cpps/pause_display.cpp
@@ -145,6 +145,7 @@ void PauseDisplay::tick() {
     if (resume_button && resume_button->IsClicked()) {
         if (game.paused_display) {
             game.request_display_change(std::move(game.paused_display));
+            game.reverting = true;
         }
     }
 
