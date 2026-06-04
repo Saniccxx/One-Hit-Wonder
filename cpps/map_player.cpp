9ad8468adef6374f82d276ac695fa20ab34efe9f
lolekszcz <lolo.szczygiel@outlook.com>
2026-06-04 15:00:20 +0200

    tp

--- cpps/map_player.cpp
+++ cpps/map_player.cpp
@@ -33,6 +33,10 @@ void MapPlayer::tick(float delta_time) {
 
 	x += dx * speed * delta_time;
 	y += dy * speed * delta_time;
+	if (x>1000) {
+		x=0;
+	}
+
 
 	const Texture2D* texture = nullptr;
 	bool mirror_x = false;
