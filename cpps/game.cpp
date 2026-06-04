91d11cfb70c3694732d5115ffa48f29305f0d381
SURFACE <malyszektobiasz@gmail.com>
2026-06-04 23:13:19 +0200

    tutorial looks better

--- cpps/game.cpp
+++ cpps/game.cpp
@@ -211,7 +211,7 @@ void Game::init() {
             50, 50, 50, 50, 50, 50, 50, 100,  50, 50, 50, 50, 50, 50, 50, 50, 150
         }
     };
-    level=notes.size();
+    level=3.0f;
 
 
     interaction_objects.push_back(std::move(std::make_unique<InteractionObject>(
